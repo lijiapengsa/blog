a75b3eecf7f3363b712814bde51c10f944880a32
lijiapeng <lijiapengas@gmail.com>
2018-08-10 20:07:22 +0800

    为了安全webhook的url动态生成

--- webhooks.js
+++ webhooks.js
@@ -1,11 +1,15 @@
+var port = 4002
+var domain = '5itech.club'
 var http = require('http')
 var exec = require('child_process').exec
+var url_random = Math.random().toString(36).substr(2);
+var url = "/webhooks/push/" + url_random
 
 http.createServer(function (req, res) {
-    // 该路径与WebHooks中的路径部分需要完全匹配，实现简易的授权认证。
-    if(req.url === '/webhooks/push/7jTgkpxZJN'){
-        // 如果url匹配，表示认证通过，则执行 sh ./deploy.sh
+    if(req.url === url){
+      // 该路径与WebHooks中的路径部分需要完全匹配，实现简易的授权认证。
         exec('git pull origin master && hexo g')
     }
     res.end()
-}).listen(4002)
+}).listen(port)
+console.log('Webhook runing at: http://' + domain + ':' + port + url);
